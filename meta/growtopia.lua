9fe22486a9adba64e6accc9653e9463612774ed4
terenzdev <alcazarin.terenzjan@gmail.com>
2025-08-06 08:59:24 +0800

    Added growtopia function

--- meta/growtopia.lua
+++ meta/growtopia.lua
@@ -1,2 +1,19 @@
+---@diagnostic disable: lowercase-global
+
 ---@class growtopia
----@field setWeather fun(weatherID:integer):nil     --Sets visual weather (0-74)
+---@field setWeather fun(weatherID:integer):nil
+---@field isOnPos fun(posX:integer, posY:integer):boolean
+---@field notify fun(message:string):nil
+---@field sendDialog fun(dialog:string):nil
+---@field getItemID fun(item_name:string):integer
+---@field checkInventory fun(item_id:integer):boolean
+---@field getItemName fun(item_id:integer):string
+---@field checkInventoryCount fun(item_id:integer):integer
+---@field tileChange fun(posX:integer, posY:integer, item_id:integer):boolean
+---@field warpTo fun(world_name:string):nil
+---@field enter fun(x?:integer, y?:integer):nil
+---@field enterGateway fun(x:integer, y:integer, buttonNumber:integer):nil
+---@field sendChat fun(text:string, to_client:boolean):nil
+
+---@type growtopia
+growtopia = growtopia or {}
